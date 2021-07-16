1557781bf57ab7f2c70e8164fcc623db6bd9dd73
yinpengxiao <chengqinghuanyu@sina.cn>
2021-07-16 21:06:34 +0800

    feat:ejs演示

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-07-15 21:03:11
- * @LastEditTime: 2021-07-15 22:24:46
+ * @LastEditTime: 2021-07-16 20:57:55
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /koa2-weibo-code/src/routes/index.js
@@ -9,8 +9,24 @@
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
+  //异步读取数据
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Koa 2!',
+    msg: '你好',
+    isMe: true,
+    blogList: [{
+      id: 1,
+      title: 'aaa'
+    }, {
+      id: 2,
+      title: 'bbb'
+    }, {
+      id: 3,
+      title: 'ccc'
+    }, {
+      id: 4,
+      title: 'ddd'
+    }]
   })
 })
 
